e55e321f561cf40a9504a58fdc4193129dfbc79e
newbiced0411 <jctdelacarcel@outlook.com>
2023-08-27 16:08:00 +0800

    create and read feature

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,25 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use Livewire\Component;
+use Illuminate\Support\Carbon;
 
 class Comments extends Component
 {
-    public $comments = [
-        [
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus soluta nemo aperiam amet, aliquam dolor sit natus ipsum sint temporibus pariatur repellat ducimus ut. Harum officia nesciunt maiores similique minus.',
-            'created_at' => '3 min ago',
-            'created' => 'Jane Doe',
-        ]
-    ];
+    public $comments;
+    public $newComment;
 
+    // READ
+    public function mount()
+    {
+        $initialComments = Comment::orderBy('created_at', 'desc')->get();
+        $this->comments = $initialComments;
+    }
+
+    // CREATE
     public function addComment()
     {
-        $this->comments[] = [
-            'body' => 'New Comment',
-            'created_at' => 'now',
-            'created' => 'John Doe',
-        ];
+        if ($this->newComment == '') return;
+        $comment = Comment::create(['body' => $this->newComment, 'user_id' => 1]);
+        $this->comments->prepend($comment);
+        $this->newComment = '';
     }
 
     public function render()
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -4,18 +4,19 @@
     </div>
     <div class="w-1/2 p-2 border-2 shadow-md">
         <div>
-            <div class="flex flex-row space-x-2">
-                <input type="text" name="comment" id="comment"
+            <form class="flex flex-row space-x-2" wire:submit.prevent="addComment">
+                <input type="text" name="comment" id="comment" wire:model.debounce.500ms="newComment"
                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                <button wire:click="addComment" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
-            </div>
+                <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+            </form>
         </div>
         @foreach ($comments as $comment)
             <div class="mt-4">
-                <h2 class="mb-2 font-bold">{{ $comment['created'] }} <span
-                        class="text-sm font-normal">{{$comment['created_at'] }}</span></h2>
-                <p>{{ $comment['body'] }}</p>
+                <h2 class="mb-2 font-bold">{{ $comment->user_id }} <span
+                        class="text-sm font-normal">{{ $comment->created_at->diffForHumans() }}</span></h2>
+                <p>{{ $comment->body }}</p>
             </div>
         @endforeach
-    </div>
+    </div>  
 </div>
