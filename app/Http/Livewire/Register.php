59637174936b8d0af16665519a64f763e5ebcf81
NewbieCed <jctdelacarcel@outlook.com>
2023-08-29 17:44:10 +0800

    registration component

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -6,6 +6,44 @@ use Livewire\Component;
 
 class Register extends Component
 {
+    public $form = [
+        'name' => '',
+        'email' => '',
+        'password' => '',
+        'password_confirmation' => '',
+    ];
+
+    // real time validation
+    public function updated($field)
+    {
+        $this->validateOnly($field, [
+            'form.name' => 'required',
+            'form.email' => 'required|email',
+            'form.password' => 'required|confirmed',
+            'form.password_confirmation' => 'required|same:password',
+        ], [], [
+            'form.name' => 'name',
+            'form.email' => 'email',
+            'form.password' => 'password',
+            'form.password_confirmation' => 'password confirmation',
+        ]);
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            'form.name' => 'required',
+            'form.email' => 'required|email',
+            'form.password' => 'required|confirmed',
+            'form.password_confirmation' => 'required|same:password',
+        ], [], [
+            'form.name' => 'name',
+            'form.email' => 'email',
+            'form.password' => 'password',
+            'form.password_confirmation' => 'password confirmation',
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.register');
--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -3,18 +3,30 @@
         <h1 class="text-3xl font-bold mb-4">Sign up</h1>
     </div>
     <div class="w-full">
-        <form class="w-full" wire:submit.prevent="addComment">
+        <form class="w-full" wire:submit.prevent="submit">
             <div class="mb-4">
-                <input type="text" name="name" id="name" placeholder="Name" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight ">
+                <input type="text" wire:model.debounce.1000ms="form.name" id="name" placeholder="Name" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight @error('form.name') border-red-500 @enderror">
+                @error('form.name')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
             </div>
             <div class="mb-4">
-                <input type="email" name="email" id="email" placeholder="Email" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight ">
+                <input type="email" wire:model.debounce.1000ms="form.email" id="email" placeholder="Email" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight @error('form.email') border-red-500 @enderror">
+                @error('form.email')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
             </div>
             <div class="mb-4">
-                <input type="password" name="password" id="password" placeholder="Password" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight ">
+                <input type="password" wire:model.debounce.1000ms="form.password" id="password" placeholder="Password" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight @error('form.password') border-red-500 @enderror">
+                @error('form.password')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
             </div>
             <div class="mb-4">
-                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight ">
+                <input type="password" wire:model.debounce.1000ms="form.password_confirmation" id="password_confirmation" placeholder="Confirm Password" class="w-full p-2 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight @error('form.password_confirmation') border-red-500 @enderror">
+                @error('form.password_confirmation')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
             </div>
             <button type="submit" id="register"
                 class="w-full bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition">Submit</button>
